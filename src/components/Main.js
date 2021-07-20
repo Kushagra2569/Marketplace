16bd113fda54b29a43ae105352fde31d3b9bfec0
Kushagra Gupta <kushagra.kingg@live.com>
2021-07-20 19:42:55 +0530

    Initial Design

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import TopHeader from './TopHeader'
+import Sidebar from './Sidebar'
+import Content from './Content'
 
 function Main() {
     return (
-        <div>
-           hello world 
+        <div className='main'>
+            <TopHeader />
+            <Sidebar />
+            <div className='wrapperCenter' >
+                <Content />
+            </div>
         </div>
     )
 }
---
+++ src/components/TopHeader.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function TopHeader() {
+    return (
+        <div className='topHeader'>
+        </div>
+    )
+}
+
+export default TopHeader
