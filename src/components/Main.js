c72c786463c538f7d4c5b0957b276abed36bc253
Kushagra Gupta <kushagra.kingg@live.com>
2021-07-28 01:44:34 +0530

    added logo

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,10 +2,12 @@ import React from 'react'
 import TopHeader from './TopHeader'
 import Sidebar from './Sidebar'
 import Content from './Content'
+import logo from './logo.png'
 
 function Main() {
     return (
         <div className='main'>
+            <img src={logo} className='logo'></img>
             <TopHeader />
             <Sidebar />
             <div className='wrapperCenter' >
